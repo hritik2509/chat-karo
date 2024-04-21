5a27d9da8e1a85bd7b4de7f7496ab59255981487
Ritik Nishad <hritiknishad1@gmail.com>
2024-04-21 13:08:16 +0530

    socket url updated

--- frontend/src/context/SocketContext.jsx
+++ frontend/src/context/SocketContext.jsx
@@ -15,7 +15,7 @@ export const SocketContextProvider = ({ children }) => {
 
   useEffect(() => {
     if (authUser) {
-      const socket = io("http://localhost:5000", {
+      const socket = io("https://chat-karo-19d5.onrender.com", {
         query: {
           userId: authUser._id,
         },
